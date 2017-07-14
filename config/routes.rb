69cbce94a96fb5cc2d16732f6db65a1c0abcf131
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-07-14 18:59:44 +0200

    Add API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
-  get 'dashboard/index'
+  root 'dashboard#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :events
+  end
 end
+
